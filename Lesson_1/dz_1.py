c027436d80f7ef715329b694c3d9b7ab044d2e88
alex-ush <mastak@bk.ru>
2022-03-27 23:53:07 +0300

    dz 1 done!

---
+++ Lesson_1/dz_1.py
@@ -0,0 +1,7 @@
+name = input('Как вас зовут? ')
+age = input('Сколько вам лет? ')
+age_average = 71
+age_left = age_average - int(age)
+
+print('{}, по статистике, впереди у вас еще {} лет счастливой жизни'.format(name, age_left))
+print('Писать на питоне точно успеете научиться=)')
